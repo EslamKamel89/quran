b9b4e7aa92ab1aeca8ed2f7d4d673f18672db966
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-29 15:24:37 +0200

    feat(api): add sura detail and full page endpoints to Quran API

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/quran')->group(function () {
     Route::get('/sura-index', [QuranController::class, 'suraIndex']);
+    Route::get('/sura/{id}', [QuranController::class, 'getSuraById']);
+    Route::get('/page/{page}/full', [QuranController::class, 'getFull']);
 });
